926e199300aeba539d14893792af25172aaeef54
ctertoole <tertoole@gmail.com>
2014-10-23 15:40:03 +0100

    R Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following code is for R Programming assignment #2
 
-## Write a short comment describing this function
+## The two functions detailed below will create a special vector
+## and cache the inverse value of that matrix.
+## The steps are as follows:
+## 1) makeMatrix     create the matrix
+## 2) getMatrix      get the value of a matrix
+## 3) cacheInverse   cahce the value of the inverse of the matrix
+## 4) getInverse     get the cahced value inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## creates the cached value or sets to NULL if nothing to cache
+  cache <- NULL
+  
+  ## the makeMatrix function will create/store the new matrix
+  makeMatrix <- function(newValue) {
+    x <<- newValue
+    cache <<- NULL
+  }
+  
+  ## The getMatrix function returns the newly stored matrix
+  getMatrix <- function() {
+    x
+  }
+  
+  ## cache the argument listed 
+  cacheInverse <- function(solve) {
+    cache <<- solve
+  }
+  
+  ## get the cached value 
+  getInverse <- function() {
+    cache
+  }
+  
+  ## return a list of all the functions
+  list(makeMatrix = makeMatrix, getMatrix = getMatrix, 
+       cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## the following function cacheSolve will calculate the inverse of a
+## created matrix that used the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  ## if cached value exists return that value
+  if(!is.null(inverse)) {
+    return(inverse)
+  }
+  ## if it doesn't exist get the matrix and return its inverse
+  matrixdata <- x$getMatrix()
+  inverse <- solve(matrixdata)
+  x$cacheInverse(inverse)
+  
+  ## show the results of the inverse
+  inverse
+  
 }
+
